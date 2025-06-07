f2e3699f926a569ce7df958dac37e6aa2ce00a77
Rajjadhav423 <2021bit057@sggs.ac.in>
2025-06-07 17:44:04 +0530

    seawater prediction enhance

--- src/app/seawater/page.tsx
+++ src/app/seawater/page.tsx
@@ -267,7 +267,7 @@ export default function SeawaterPage() {
         throw new Error('Salinity must be between 0-50 g/kg')
       }
 
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://ligninchatbot.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
